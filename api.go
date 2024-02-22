2d85df600645daaa93b976a15be71b9aa0b4670e
victorisena <victor.igor26@gmail.com>
2024-02-22 10:33:55 -0300

    Create our first handlers

---
+++ api.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHttpHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			//Handle the error
+			WriteJson(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type ApiServer struct {
+	listenAddress string
+}
+
+func NewApiServer(listenAddress string) *ApiServer {
+	return &ApiServer{
+		listenAddress: listenAddress,
+	}
+}
+
+func (s *ApiServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHttpHandleFunc(s.handleGetAccount))
+
+	log.Println("JSON API server running on port:", s.listenAddress)
+
+	http.ListenAndServe(s.listenAddress, router)
+}
+
+func (s *ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed %s", r.Method)
+}
+
+func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+
+	fmt.Println(id)
+
+	return WriteJson(w, http.StatusOK, &Account{})
+}
+
+func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/victorisena/gobank
+
+go 1.21.6
+
+require github.com/gorilla/mux v1.8.1
