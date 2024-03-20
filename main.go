bdbce99cede8a8fb2a664537447ed16716029fa1
victorisena <victor.igor26@gmail.com>
2024-03-20 10:47:01 -0300

    Creating our build seed

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func seedAccount(store Storage, firstName, lastName, password string) *Account {
+	acc, err := newAccount(firstName, lastName, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "Victor", "Sena", "test")
+}
 
 func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
 	store, err := newPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -10,6 +36,12 @@ func main() {
 
 	if err := store.Init(); err != nil {
 		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("seeding the database")
+		// seed stuff
+		seedAccounts(store)
 	}
 
 	server := NewApiServer(":3000", store)
--- storage.go
+++ storage.go
@@ -13,6 +13,7 @@ type Storage interface {
 	UpdateAccount(*Account) error
 	GetAccounts() ([]*Account, error)
 	GetAccountById(int) (*Account, error)
+	GetAccountByNumber(int) (*Account, error)
 }
 
 type PostgresStore struct {
@@ -58,7 +59,7 @@ func (s *PostgresStore) CreateAccount(acc *Account) error {
 	(first_name, last_name, number, balance, created_at)
 	values ($1, $2, $3, $4, $5)`
 
-	resp, err := s.db.Query(
+	_, err := s.db.Query(
 		query,
 		acc.FirstName,
 		acc.LastName,
@@ -69,14 +70,25 @@ func (s *PostgresStore) CreateAccount(acc *Account) error {
 		return err
 	}
 
-	fmt.Printf("%v\n", resp)
-
 	return nil
 }
 
 func (s *PostgresStore) DeleteAccount(id int) error {
 	_, err := s.db.Query("delete from account where id = $1", id)
 	return err
+}
+
+func (s *PostgresStore) GetAccountByNumber(number int) (*Account, error) {
+	rows, err := s.db.Query("select * from account where number = $1", number)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		return scanIntoAccount(rows)
+	}
+
+	return nil, fmt.Errorf("account with number [%d] not found", number)
 }
 
 func (s *PostgresStore) UpdateAccount(*Account) error {
